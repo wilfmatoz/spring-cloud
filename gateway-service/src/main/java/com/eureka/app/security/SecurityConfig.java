169a87bcfe4d2de53a2ac46c4190c2ae99d71940
Deynner Mateus <wilfmatoz@gmail.com>
2022-06-19 20:32:55 -0500

    keycloak

---
+++ gateway-service/src/main/java/com/eureka/app/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.eureka.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login(Customizer.withDefaults());
+        http.csrf().disable();
+        return http.build();
+    }
+
+}
